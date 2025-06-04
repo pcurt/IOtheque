099122a93d6780351fcb09b8c23588f7dfffb61d
Pierrick Curt <pierrickcurt@gmail.com>
2025-06-04 12:52:44 +0200

    feat: logo refactor

--- components/generalElements/Logo.vue
+++ components/generalElements/Logo.vue
@@ -2,14 +2,15 @@
 const runtimeConfig = useRuntimeConfig();
 const img = useImage();
 
-const logoUrl = runtimeConfig?.public?.LOGO ? img(runtimeConfig?.public?.LOGO) : "null";
+const logo = "https://back.iotheque.com/wp-content/uploads/2025/06/logo_iotheque_books@4x.png"
+const name = "https://back.iotheque.com/wp-content/uploads/2025/06/logo_iotheque_text@4x.png"
 </script>
 
 <template>
   <NuxtLink to="/">
     <div class="flex items-center gap-2 text-lg font-bold">
-      <img :src="logoUrl" alt="Logo" class="object-contain h-10" />
-      <span>IOTheque</span>
+      <img :src="logo" alt="Logo" class="object-contain h-10" />
+      <span><img :src="name" alt="Logo" class="object-contain h-8" /></span>
     </div>
   </NuxtLink>
 </template>
